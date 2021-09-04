cbe4f0d388456b95fb6e49107d8e485e5939ce72
ErnestoLz <ernestolopez2018@fau.edu>
2021-09-04 19:25:32 -0400

    added bio & image to Ernesto/Ernesto.css and Image folder

--- src/Ernesto.js
+++ src/Ernesto.js
@@ -1,7 +1,9 @@
 /*import logo from './logo.svg';*/
 import './App.css';
+import './Ernesto.css'
 import React from 'react';
 import {Link } from "react-router-dom";
+import photo from './Images/e_image.jpg';
 
 function Ernesto() {
   return (
@@ -11,14 +13,20 @@ function Ernesto() {
         
       
 
+        <body>
+        <img src={photo} className='photo' alt='headshot'/>
         <h1>
          Welcome to Ernesto's page
         </h1>
-        <p style={{fontSize: '22px'}}>
-          I am a Senior at FAU studying Computer Science expecting to graduate after Spring 2022
+        <p className='bio'>
+          I am a Senior at FAU studying Computer Science expecting to graduate after Spring 2022,
+          I enjoy playing video games and traveling with close friends. I used to play the Cello for
+          over 7 years and have recently made it a hobby of mine.
         </p>
 
-        <Link to="/Home"><button style={{height: '30px', width : '100px'}}>Homepage</button></Link>
+        </body>
+
+        <Link to="/"><button style={{height: '30px', width : '100px'}}>Homepage</button></Link>
         
         
       
