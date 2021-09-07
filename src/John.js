ebef2883f52db9346d6b17050eb0a88602333323
JSerrano98 <jserrano2019@fau.edu>
2021-09-06 22:50:31 -0400

    John about page updated

--- src/John.js
+++ src/John.js
@@ -1,4 +1,7 @@
+import logo from './logo.svg';
 import './App.css';
+import './App.js';
+import john from './Images/JohnS.jpg';
 import {Link } from "react-router-dom";
 
 function John() {
@@ -8,21 +11,23 @@ function John() {
         <h1>
           John Serrano
         </h1>
+        <img style={{height: '400px',width: '400px'}}src={john} alt='john'/>
        <h2>
        About me
        </h2>
        
         <p>
-         I am currently a senior pursuing my bachelor's in Computer Engineering at FAU. I like to play Video games in the 
-         spare time that I have. I also enjoy reading as well, and have develped a big mystery kick here lately.
-          Beyond that I'm working to expand my programming skills, and teaching myself things outside of class. I'm currently sceduled to graduat in spring 2022.
+        I am currently a senior pursuing my bachelor's in Computer Engineering at FAU. I like to play Video games in the 
+         spare time that I have. I also enjoy reading as well and have developed a big mystery kick here lately.
+          Beyond that I'm working to expand my programming skills and teaching myself things outside of class. I'm currently sceduled to graduate in spring 2022.
+
         </p>
         
         <Link to="/">
           <button style={{height: '30px', width: '100px'}}>Homepage</button>
         </Link>
 
-        
+
       </header>
     </div>
   );
